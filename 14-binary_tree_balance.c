850ac247f8a95481103b7573f578a9108fe9dcf6
ritaOmondi <cherylritaomondi@gmail.com>
2024-01-30 02:01:15 -0800

    the balancing factor

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,34 +1,41 @@
 #include "binary_trees.h"
-/**
- * binary_tree_height - Measures the height of a binary tree
- * @tree: Pointer to the root node of the tree
- *
- * Return: Height of the tree, or 0 if tree is NULL
- */
-size_t binary_tree_height(const binary_tree_t *tree)
-{
-if (tree == NULL)
-return (0);
-size_t left_height = binary_tree_height(tree->left);
-size_t right_height = binary_tree_height(tree->right);
-
-return ((left_height > right_height ? left_height : right_height) + 1);
-}
 
 /**
- * binary_tree_balance - Measures the balance factor of a binary tree
- * @tree: Pointer to the root node of the tree to measure the balance factor
+ * binary_tree_balance - measures the balance factor of a binary tree
+ * @tree: pointer to the root node of the tree to measure the balance factor
  *
- * Return: Balance factor of the tree, or 0 if tree is NULL
+ * Return: the balance factor
+ *         0 if tree is NULL
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-if (tree == NULL)
-	return (0);
+	int height_l, height_r;
 
-int left_height = binary_tree_height(tree->left);
-int right_height = binary_tree_height(tree->right);
+	if (!tree)
+		return (0);
 
-return (left_height - right_height);
+	height_l = tree->left ? (int)binary_tree_height(tree->left) : -1;
+	height_r = tree->right ? (int)binary_tree_height(tree->right) : -1;
+
+	return (height_l - height_r);
 }
 
+/**
+ * binary_tree_height - measures the height of a binary tree
+ * @tree: tree to measure the height of
+ *
+ * Return: height of the tree
+ *         0 if tree is NULL
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t height_l = 0;
+	size_t height_r = 0;
+
+	if (!tree)
+		return (0);
+
+	height_l = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+	height_r = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+	return (height_l > height_r ? height_l : height_r);
+}
